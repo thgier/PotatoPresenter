1582d47cca910e1570f3db964a8ff58f05b2598c
Theresa Gier <theresa@fam-gier.de>
2021-02-23 16:08:21 +0100

    change comments in Frame and Presentation

--- src/core/frame.h
+++ src/core/frame.h
@@ -13,7 +13,7 @@ public:
     Frame();
     Frame(QString const& id, std::map<QString, QString> const& variables);
 
-//    Access contained boxes
+    // Access contained boxes
     void setBoxes(std::vector<std::shared_ptr<Box>> boxes);
     void appendBox(std::shared_ptr<Box> box);
     Box::List boxes() const;
@@ -21,19 +21,19 @@ public:
     Box::Ptr findBox(QString const& id) const;
     bool containsBox(QString const& id) const;
 
-//    Returns the max PauseCounter of the boxes
+    // Returns the max PauseCounter of the boxes
     int numberPauses() const;
 
-//    Template boxes are rendered in the background of the frame.
+    // Template boxes are rendered in the background of the frame.
     void setTemplateBoxes(Box::List boxes);
     void appendTemplateBoxes(Box::Ptr box);
     Box::List templateBoxes() const;
 
-//    The frame ID is the string after the "\frame" command, and is used to track
-//    the frame when the document changes.
+    // The frame ID is the string after the "\frame" command, and is used to track
+    // the frame when the document changes.
     QString id() const;
 
-//    Access to this frame's variables
+    // Access to this frame's variables
     void setVariables(Variables const& variables);
     void setVariable(QString const& name, QString const& value);
     Variables const& variables() const;
--- src/core/presentation.h
+++ src/core/presentation.h
@@ -14,7 +14,7 @@ public:
     Presentation();
     void loadInput(QString configFilename);
 
-//    Access contained Frames
+    // Access contained Frames
     Frame::List frames() const;
     void setFrames(Frame::List const& frames);
     Frame::Ptr frameAt(int pageNumber) const;
@@ -24,16 +24,16 @@ public:
 
     Box::Ptr findBox(QString const& id) const;
 
-//    Change Geometry of Box only through the presentation in order to
-//    save it in the Configuration
+    // Change Geometry of Box only through the presentation in order to
+    // save it in the Configuration
     void setBoxGeometry(QString const& boxId, BoxGeometry const& rect, int pageNumber);
 
-//    Configuration Class to follow and save the Geometry of the boxes
+    // Configuration Class to follow and save the Geometry of the boxes
     void saveConfig(QString const& file);
     ConfigBoxes& configuration();
 
-//    Layout class has the information about the positions of the predefined boxes
-//    avaible throught the tool bar e.g. the position of the title and the body
+    // Layout class has the information about the positions of the predefined boxes
+    // avaible throught the tool bar e.g. the position of the title and the body
     void setLayout(Layout layout);
     Layout layout() const;
 
