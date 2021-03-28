8ee0a26f40e002739f94e54328e1ef88ef1c7598
Theresa Gier <theresa@fam-gier.de>
2021-03-28 19:49:50 +0200

    add license

--- src/core/sliderenderer.h
+++ src/core/sliderenderer.h
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2020-2021 Theresa Gier <theresa@fam-gier.de>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
 #ifndef PAINTER_H
 #define PAINTER_H
 #include "slide.h"
--- src/core/template.h
+++ src/core/template.h
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2020-2021 Theresa Gier <theresa@fam-gier.de>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
 # pragma once
 
 #include <QPainter>
